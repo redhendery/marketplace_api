e99afc553af28d30beee0d61b9f33a154b332c59
redhendery <redhendery@gmail.com>
2019-08-29 12:13:03 -0700

    add tests and methods to CRUD users

---
+++ test/models/user_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+  test 'user with valid email should be valid' do
+    user = User.new(email: 'test@test.com', password_digest: 'test')
+    assert user.valid?
+  end
+
+  test 'user with invalid email should be invalid' do
+    user = User.new(email: 'notanemail', password_digest: 'test')
+    assert_not user.valid?
+  end
+
+  test 'user email address should be unique' do
+    other_user = users(:one)
+    user = User.new(email: other_user.email, password_digest: 'test')
+    assert_not user.valid?
+  end
+end
